74be2ec73c1deb3246c6a686c7de5556858a7ace
Adam Harries <adam.harries@ed.ac.uk>
2017-07-18 17:29:42 +0100

    fix build error from using incorrect macro

--- src/sparse_matrix.cpp
+++ src/sparse_matrix.cpp
@@ -60,7 +60,7 @@ void SparseMatrix::load_from_file(std::string filename) {
       I--;
       J--;
       nz_entries.push_back(std::make_tuple(I, J, val));
-      if(mm_set_symmetric(matcode)){
+      if(mm_is_symmetric(matcode)){
         nz_entries.push_back(std::make_tuple(J, I, val));
       }
       if (i == 0) {
